fb0d04c84164b8788ac2a3f6a7f8f18fb53cd19b
psj1997 <1176894699@qq.com>
2022-02-28 15:41:21 +0800

    remove play

--- example.js
+++ example.js
@@ -32,7 +32,7 @@ function statement (invoices, plays){
         {style: "currency", currency: 'USD',
         minimumFractionDigits: 2}).format;
     for (let perf of invoices.performances) {
-        const play = plays[perf.playID];
+        const play = playFor(perf);
         let thisAmount = amountFor(perf, play);
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0)
@@ -47,21 +47,25 @@ function statement (invoices, plays){
     return result
 }
 
-function amountFor(perf, play){
+function playFor(aPerformance) {
+    return plays[aPerformance.playID]
+}
+
+function amountFor(aPerformance, play){
     let result = 0;
     switch (play.type) {
         case "tragedy":
             result = 40000;
-            if (perf.audience > 30){
-                result += 1000 * (perf.audience - 30);
+            if (aPerformance.audience > 30){
+                result += 1000 * (aPerformance.audience - 30);
             }
             break;
         case "comedy":
             result = 30000;
-            if (perf.audience > 20){
-                result += 10000 + 500 * (perf.audience - 20);
+            if (aPerformance.audience > 20){
+                result += 10000 + 500 * (aPerformance.audience - 20);
             }
-            result += 300 * perf.audience;
+            result += 300 * aPerformance.audience;
             break;
         default:
             throw new Error(`unknown type: ${play.type}`);
