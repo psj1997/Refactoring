6fec7bc29743f8e33e9e65a3d637fd4aab294d57
psj1997 <1176894699@qq.com>
2022-02-28 15:49:28 +0800

    split loop

--- example.js
+++ example.js
@@ -26,12 +26,15 @@ invoices =
 
 function statement (invoices, plays){
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `Statement for ${invoices.customer}\n`
     for (let perf of invoices.performances) {
-        volumeCredits = volumeCreditsFor(perf);
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
+    }
+
+    let volumeCredits = 0;
+    for (let perf of invoices.performances) {
+        volumeCredits = volumeCreditsFor(perf);
     }
     result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
