5f541eb8f54b4411220ef93723beba3166d53c2b
psj1997 <1176894699@qq.com>
2022-02-28 15:48:22 +0800

    rename format to usd

--- example.js
+++ example.js
@@ -30,18 +30,18 @@ function statement (invoices, plays){
     let result = `Statement for ${invoices.customer}\n`
     for (let perf of invoices.performances) {
         volumeCredits = volumeCreditsFor(perf);
-        result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+        result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result
 }
 
-function format(aNumber){
+function usd(aNumber){
     return new Intl.NumberFormat("en-US",
         {style: "currency", currency: 'USD',
-        minimumFractionDigits: 2}).format(aNumber);
+        minimumFractionDigits: 2}).format(aNumber/100);
 }
 
 function volumeCreditsFor(perf){
